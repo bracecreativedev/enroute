6ebe0dbd0362dfa8b3b3c9c907212ba5e5fcb182
mattdobsonweb <mpd.mattdobson@icloud.com>
2018-11-16 10:19:24 +0000

    Updates keys file

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,8 +1,1 @@
-module.exports = {
-  MongoURI: process.env.MONGO_URI,
-  key: process.env.SECRET,
-  OMDBkey: process.env.OMDB_KEY,
-  TMDBkey: process.env.TMDB_KEY,
-  backdropImagePath: "https://image.tmdb.org/t/p/w1280",
-  posterImagePath: "https://image.tmdb.org/t/p/w500"
-};
+module.exports = {};
