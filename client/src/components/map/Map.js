d8dbdbd0af1527d32dd4135c5c24cc79a21bcb7d
mattdobsonweb <mpd.mattdobson@icloud.com>
2018-12-20 11:27:43 +0000

    Checking key

--- client/src/components/map/Map.js
+++ client/src/components/map/Map.js
@@ -31,6 +31,8 @@ class Map extends Component {
   componentDidMount() {
     const { isAuthenticated } = this.props.auth;
 
+    console.log({ key: 'process.env.REACT_APP_GOOGLE_KEY' });
+
     this.props.getLocations();
     this.props.setFeaturedLocation();
 
